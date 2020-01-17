51cc21fe608067ad009b9fe673f8ab9879cf37db
Mochalin Arseniy <coddeboy@gmail.com>
2020-01-17 17:09:54 +0300

    Improve code

--- BuilderBot/EV3DEV Python/RoboK2020.py
+++ BuilderBot/EV3DEV Python/RoboK2020.py
@@ -3,16 +3,14 @@ import time
 
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, MoveSteering, MoveTank
 from ev3dev2.sensor.lego import ColorSensor
-from ev3dev2.sensor import INPUT_4, INPUT_1
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
 from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
 from ev3dev2.button import Button
 
 # ------------------------------PINS------------------------------
-motor_l = LargeMotor(OUTPUT_A)
-motor_r = LargeMotor(OUTPUT_D)
-motors = MoveSteering(OUTPUT_D, OUTPUT_A)
-motors_t = MoveTank(OUTPUT_D, OUTPUT_A)
+pin_motors = {'LEFT': OUTPUT_A,
+              'RIGHT': OUTPUT_D}
 
 sens_line = ColorSensor(INPUT_1)
 sens_cross = ColorSensor(INPUT_4)
@@ -20,7 +18,7 @@ sens_cross = ColorSensor(INPUT_4)
 # ------------------------------CONSTANS------------------------------
 # Movement
 wheel_radius = 55 // 2
-rotate_radius = 160 // 2
+rotate_radius = 150 // 2
 motor_speed = -60
 cross_center_dist = 120
 dist_to_cube = 335 - cross_center_dist
@@ -35,89 +33,106 @@ kp = 0.9
 
 
 # ------------------------------DEBUG------------------------------
-def beep():
-    Sound().beep()
+class Debug:
+    @staticmethod
+    def beep():
+        Sound().beep()
 
+    @staticmethod
+    def say(text='Welcome to DEBUG!'):
+        Sound().speak(text, volume=80)
 
-def say(text='Welcome to DEBUG!'):
-    Sound().speak(text, volume=80)
+    @staticmethod
+    def blink(clr_l, clr_r):
+        Leds().set_color('LEFT', clr_l)
+        Leds().set_color('RIGHT', clr_r)
 
-
-def blink(clr_l, clr_r):
-    Leds().set_color('LEFT', clr_l)
-    Leds().set_color('RIGHT', clr_r)
-
-
-def wait_for_key():
-    blink('AMBER', 'AMBER')
-    Button().wait_for_bump('enter')
-    blink('GREEN', 'GREEN')
+    def wait_for_key(self):
+        self.blink('AMBER', 'AMBER')
+        Button().wait_for_bump('enter')
+        self.blink('GREEN', 'GREEN')
 
 
 # ------------------------------VOID------------------------------
-def len_to_degrees(length):
-    return 180 * length // wheel_radius * 3.14
 
 
 # ---------MOTORS-------------
-def rotate_90_left():
-    angle = 90 * rotate_radius // wheel_radius  # 2*pi*R * 1/4 // 2 * pi* r // 90
-    motors.on_for_degrees(steering=-100, speed=motor_speed, degrees=angle)
+class MotorsController:
+    def __init__(self):
+        self.motor_left = LargeMotor(pin_motors['LEFT'])
+        self.motor_right = LargeMotor(pin_motors['RIGHT'])
+        self.motors_s = MoveSteering(pin_motors['LEFT'], pin_motors['RIGHT'])
+        self.motors_t = MoveTank(pin_motors['LEFT'], pin_motors['RIGHT'])
+
+    def get_motor_enc(self, motor='LEFT'):
+        if motor == 'LEFT':
+            return self.motor_left.position_sp
+        else:
+            return self.motor_right.position_sp
+
+    def rotate_90_left(self):
+        angle = 90 * rotate_radius // wheel_radius  # 2*pi*R * 1/4 // 2 * pi* r // 90
+        self.motors_s.on_for_degrees(steering=-100, speed=motor_speed, degrees=angle)
+
+    def rotate_90_right(self):
+        angle = 90 * rotate_radius // wheel_radius
+        self.motors_s.on_for_degrees(steering=100, speed=motor_speed, degrees=angle)
+
+    def rotate_180_left(self):
+        angle = 180 * rotate_radius // wheel_radius
+        self.motors_s.on_for_degrees(steering=-100, speed=motor_speed, degrees=angle)
+
+    def rotate_180_right(self):
+        angle = 180 * rotate_radius // wheel_radius
+        self.motors_s.on_for_degrees(steering=100, speed=motor_speed, degrees=angle)
+
+    def move_to_cross_center(self):
+        angle = 180 * cross_center_dist // wheel_radius * 3.14
+        self.motors_s.on_for_degrees(steering=0, speed=motor_speed, degrees=angle)
 
 
-def rotate_90_right():
-    angle = 90 * rotate_radius // wheel_radius  # 2*pi*R * 1/4 // 2 * pi* r // 90
-    motors.on_for_degrees(steering=100, speed=motor_speed, degrees=angle)
-
-
-def rotate_180_left():
-    angle = 2 * 90 * rotate_radius // wheel_radius  # 2*pi*R * 1/4 // 2 * pi* r // 90
-    motors.on_for_degrees(steering=-100, speed=motor_speed, degrees=angle)
-
-
-def rotate_180_right():
-    angle = 2 * 90 * rotate_radius // wheel_radius  # 2*pi*R * 1/4 // 2 * pi* r // 90
-    motors.on_for_degrees(steering=100, speed=motor_speed, degrees=angle)
-
-
-def move_to_cross_center():
-    motors.on_for_degrees(steering=0, speed=motor_speed, degrees=len_to_degrees(cross_center_dist))
-
-
-def line_move(limit=0):
-    motor_l_enc = motor_l.position_sp
-    motor_r_enc = motor_r.position_sp
-    condition = (sens_cross.reflected_light_intensity > middle_val) if limit == 0 else \
-        (motor_l.position_sp - motor_l_enc < limit and motor_r.position_sp - motor_r_enc < limit)
+def line_move(mc, distantion=0):
+    # Save initially values on encoders(in degrees)
+    motor_l_enc = mc.get_motor_enc('LEFT')
+    motor_r_enc = mc.get_motor_enc('RIGHT')
+    # Cross or distantion
+    condition = (sens_cross.reflected_light_intensity > middle_val) if distantion == 0 else \
+        (mc.get_motor_enc('LEFT') - motor_l_enc < distantion and mc.get_motor_enc('RIGHT') - motor_r_enc < distantion)
     while condition:
+        # Calculate PID(P) value
         p_val = (sens_line.reflected_light_intensity - middle_val - middle_val) * kp
+        # Run motors with speed by PID(P) value
         speed_l = p_val if p_val < 0 else -p_val
-        motors_t.on(speed_l, motor_speed)
-
-    motors_t.stop()
+        mc.motors_t.on(speed_l, motor_speed)
+    # Stop motors after cross/distantion_limit
+    mc.motors_t.stop()
 
 
 # ------------------------------MAIN------------------------------
 def setup():
-    blink('GREEN', 'GREEN')
+    Debug.blink('GREEN', 'GREEN')
 
 
 def main():
     setup()
+    mc = MotorsController()
 
-    line_move()
-    rotate_90_left()
-    line_move(dist_to_cube)
-    rotate_180_right()
-    line_move()
-    rotate_90_left()
-    while Button().buttons_pressed != ['backspace']:
-        print('OK')
-        time.sleep(1)
+    line_move(mc)
+    Debug.beep()
+    Button().wait_for_bump(['enter'])
+    mc.rotate_180_right()
+    Debug.beep()
+    Button().wait_for_bump(['enter'])
+    mc.rotate_90_left()
+
+    Debug.beep()
+    while Button().buttons_pressed != ['up']:
+        Sound().play_file('/home/robot/troll.wav', 70)
+        print('HA-HA!')
 
 
 if __name__ == '__main__':
     while True:
-        blink('RED', 'RED')
+        Debug.blink('RED', 'RED')
         Button().wait_for_bump('enter')
         main()
